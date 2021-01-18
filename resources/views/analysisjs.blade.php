b5d46472efe4870a7f142ef210b0bf897c80a585
Kyle <kyle.ooi97@gmail.com>
2021-01-18 13:25:56 +0800

    Created 6 charts for patient analysis

--- resources/views/analysisjs.blade.php
+++ resources/views/analysisjs.blade.php
@@ -1,5 +1,5 @@
 <script>
-    google.charts.load('current', {'packages':['corechart']});
+    google.charts.load('current', {'packages':['corechart','controls']});
 
 // Set a callback to run when the Google Visualization API is loaded.
 google.charts.setOnLoadCallback(drawChart);
@@ -8,27 +8,271 @@ google.charts.setOnLoadCallback(drawChart);
 // instantiates the pie chart, passes in the data and
 // draws it.
 function drawChart() {
+    drawBodyPartsCountChart();
+    drawDescriptionCountChart();
+    drawPainLevelChart();
+    drawTopPainChart();
+    drawMoodChart();
+    drawDurationChart();
+}
 
-// Create the data table.
-var data = new google.visualization.DataTable();
-data.addColumn('string', 'Topping');
-data.addColumn('number', 'Slices');
-data.addRows([
-    ['Mushrooms', 3],
-    ['Onions', 1],
-    ['Olives', 1],
-    ['Zucchini', 1],
-    ['Pepperoni', 2]
-]);
+function drawBodyPartsCountChart(){
+    var bodyPartsCountData = google.visualization.arrayToDataTable([
+        ['body parts','count'],
 
-// Set chart options
-var options = {'title':'How Much Pizza I Ate Last Night',
-                'width':400,
-                'height':300};
+        @php
+        foreach($bodyPartsCount as $item) {
+            echo "['".$item->body_part."',parseInt('".$item->count."')],";
+        }
+        @endphp
+        ]);
 
-// Instantiate and draw our chart, passing in some options.
-var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-chart.draw(data, options);
+    // Set chart options
+    var bodyPartsCountOptions = {'title':'sample',
+                    'width':600,
+                    'height':300};
+
+    // Instantiate and draw our chart, passing in some options.
+    var bodyPartsCountChart = new google.visualization.BarChart(document.getElementById('body-parts-chart-div'));
+    bodyPartsCountChart.draw(bodyPartsCountData, bodyPartsCountOptions);
+}
+
+function drawDescriptionCountChart(){
+    var descriptionCountData = google.visualization.arrayToDataTable([
+        ['body parts','description','count'],
+
+        @php
+        foreach($descriptionCount as $item) {
+            echo "['".$item->body_part."','".$item->description."',parseInt('".$item->count."')],";
+        }
+        @endphp
+    ]);
+
+    // Create a dashboard.
+    var dashboard = new google.visualization.Dashboard(
+            document.getElementById('dashboard_div'));
+
+    var bodyPartSelector = new google.visualization.ControlWrapper({
+        'controlType': 'CategoryFilter',
+        'containerId': 'filter_div',
+        'options': {
+        'filterColumnLabel': 'body parts'
+        }
+    });
+
+    var pieChart = new google.visualization.ChartWrapper({
+        'chartType': 'PieChart',
+        'containerId': 'chart_div',
+        'dataTable':descriptionCountData,
+        'options': {
+            'width': 300,
+            'height': 300,
+            'title': 'Pain description count'
+        },
+        // The pie chart will use the columns 'Name' and 'Donuts eaten'
+        // out of all the available ones.
+        'view': {'columns': [ 1, 2]}
+    });
+
+    // Establish dependencies, declaring that 'filter' drives 'pieChart',
+    // so that the pie chart will only display entries that are let through
+    // given the chosen slider range.
+    dashboard.bind(bodyPartSelector, pieChart);
+
+    dashboard.draw(descriptionCountData);
+}
+
+function drawPainLevelChart(){
+     // third chart
+
+    var levelData = google.visualization.arrayToDataTable([
+        ['date','level'],
+
+        @php
+        foreach($reports as $item) {
+            // echo "[new Date('".$item->created_at."'.substr(0,10)),parseInt('".$item->level."')],";
+            echo "[new Date('".$item->created_at."'),parseInt('".$item->level."')],";
+        }
+        @endphp
+    ]);
+
+    // Create a dashboard.
+    var dashboard2 = new google.visualization.Dashboard(
+            document.getElementById('dashboard2_div'));
+
+    var dateRangeFilter = new google.visualization.ControlWrapper({
+        'controlType': 'ChartRangeFilter',
+        'containerId': 'filter2_div',
+        'options': {
+        'filterColumnLabel': 'date',
+            ui:{
+                chartOptions: {
+                    height:50,
+                    width:600,
+                }
+            }
+        }
+    });
+
+    
+
+    var lineChart = new google.visualization.ChartWrapper({
+        'chartType': 'LineChart',
+        'containerId': 'chart2_div',
+        'dataTable':levelData,
+        'options': {
+            'width': 900,
+            'height': 500,
+            'title': 'Level of pain'
+        },
+    });
+
+    // Establish dependencies, declaring that 'filter' drives 'pieChart',
+    // so that the pie chart will only display entries that are let through
+    // given the chosen slider range.
+    dashboard2.bind(dateRangeFilter, lineChart);
+
+    dashboard2.draw(levelData);
+
+}
+
+function drawTopPainChart(){
+       // forth chart
+
+    var highestPainData = google.visualization.arrayToDataTable([
+        ['description','level','date','body part'],
+
+        @php
+        foreach($highestPainLevel as $item) {
+            echo "['".$item->description."',parseInt('".$item->level."'),new Date('".$item->created_at."'),'".$item->body_part."'],";
+        }
+        @endphp
+    ]);
+
+     // Create a dashboard.
+    var highestPainDashboard = new google.visualization.Dashboard(
+            document.getElementById('dashboard3_div'));
+
+    var dateRangeFilter2 = new google.visualization.ControlWrapper({
+        'controlType': 'DateRangeFilter',
+        'containerId': 'filter3_div',
+        'options': {
+        'filterColumnLabel': 'date'
+        }
+    });
+
+    var filter2 = new google.visualization.ControlWrapper({
+        'controlType': 'CategoryFilter',
+        'containerId': 'filter3_1_div',
+        'options': {
+        'filterColumnLabel': 'body part'
+        }
+    });
+    
+    var highestPainBarChart = new google.visualization.ChartWrapper({
+        'chartType': 'BarChart',
+        'containerId': 'highestPainDataChart_div',
+        'dataTable':highestPainData,
+        'options': {
+            'width': 500,
+            'height': 300,
+            'title': 'Pain description count'
+        },
+        'view':{'columns':[0,1]}
+    });
+
+     // Establish dependencies, declaring that 'filter' drives 'pieChart',
+    // so that the pie chart will only display entries that are let through
+    // given the chosen slider range.
+    highestPainDashboard.bind(dateRangeFilter2, highestPainBarChart);
+    highestPainDashboard.bind(filter2, highestPainBarChart);
+
+    highestPainDashboard.draw(highestPainData);
+}
+
+function drawMoodChart(dashboard, filter){
+       var data = google.visualization.arrayToDataTable([
+        ['date','mood'],
+
+        @php
+        foreach($reports as $item) {
+            echo "[new Date('".$item->created_at."'),parseInt('".$item->mood."')],";
+        }
+        @endphp
+    ]);
+
+     // Create a dashboard.
+    var dashboard = new google.visualization.Dashboard(
+            document.getElementById('dashboard4_div'));
+
+    var filter = new google.visualization.ControlWrapper({
+        'controlType': 'DateRangeFilter',
+        'containerId': 'filter4_div',
+        'options': {
+        'filterColumnLabel': 'date'
+        }
+    });
+    
+    var chart = new google.visualization.ChartWrapper({
+        'chartType': 'LineChart',
+        'containerId': 'chart4_div',
+        'dataTable':data,
+        'options': {
+            'width': 500,
+            'height': 300,
+            'title': 'Pain description count'
+        },
+    });
+
+     // Establish dependencies, declaring that 'filter' drives 'pieChart',
+    // so that the pie chart will only display entries that are let through
+    // given the chosen slider range.
+    dashboard.bind(filter, chart);
+
+    dashboard.draw(data);
+}
+
+function drawDurationChart(){
+    var data = google.visualization.arrayToDataTable([
+        ['body part','duration','average','date'],
+
+        @php
+        foreach($durationPerBodyPart as $item) {
+            echo "['".$item->body_part."',parseInt('".$item->duration."'),parseFloat('".$item->average."'),new Date('".$item->created_at."')],";
+        }
+        @endphp
+    ]);
+
+     // Create a dashboard.
+    var dashboard = new google.visualization.Dashboard(
+            document.getElementById('dashboard5_div'));
+
+    var filter = new google.visualization.ControlWrapper({
+        'controlType': 'DateRangeFilter',
+        'containerId': 'filter5_div',
+        'options': {
+        'filterColumnLabel': 'date'
+        }
+    });
+    
+    var chart = new google.visualization.ChartWrapper({
+        'chartType': 'BarChart',
+        'containerId': 'chart5_div',
+        'dataTable':data,
+        'options': {
+            'width': 500,
+            'height': 300,
+            'title': 'Pain description count'
+        },
+        'view': {'columns': [0,1,2]}
+    });
+
+     // Establish dependencies, declaring that 'filter' drives 'pieChart',
+    // so that the pie chart will only display entries that are let through
+    // given the chosen slider range.
+    dashboard.bind(filter, chart);
+
+    dashboard.draw(data);
 }
 </script>
 <p>hello</p>
--- resources/views/patients/analyse.blade.php
+++ resources/views/patients/analyse.blade.php
@@ -17,12 +17,44 @@ use App\Common;
                     </div>
                     @endif
                     <p>Analysis</p>
-                    <div id="chart_div"></div>
+                    <div id="body-parts-chart-div"></div>
+                    <div id="dashboard_div">
+                        <!--Divs that will hold each control and chart-->
+                        <div id="filter_div"></div>
+                        <div id="chart_div"></div>
+                    </div>
+
+                    <div id="dashboard2_div">
+                        <!--Divs that will hold each control and chart-->
+                        <div id="filter2_div"></div>
+                        <div id="chart2_div"></div>
+                    </div>
+
+                    <div id="dashboard3_div">
+                        <!--Divs that will hold each control and chart-->
+                        <div id="filter3_div"></div>
+                        <div id="filter3_1_div"></div>
+                        <div id="highestPainDataChart_div"></div>
+                    </div>
+
+                    <div id="dashboard4_div">
+                        <!--Divs that will hold each control and chart-->
+                        <div id="filter4_div"></div>
+                        <div id="chart4_div"></div>
+                    </div>
+
+                    <div id="dashboard5_div">
+                        <!--Divs that will hold each control and chart-->
+                        <div id="filter5_div"></div>
+                        <div id="chart5_div"></div>
+                    </div>
+
+                    @include('analysisjs')
                 </div>
             </div>
         </div>
     </div>
 </div>
 
-{{--  --}}
+
 @endsection
