8f5a9c05c3a74af7047d6c2d4be489ff90237843
Kyle <kyle.ooi97@gmail.com>
2021-01-19 15:26:54 +0800

    fix main routing to login page and added all authentication to prevent user to access without login

--- routes/web.php
+++ routes/web.php
@@ -14,20 +14,15 @@ use App\Http\Controller\PatientController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-
-Route::get('/patients/search/', 'PatientController@search')->name('patients.search');
-Route::get('/patients/{id}/analyse','PatientController@analyse')->name('patients.analyse');
-Route::resource('patients', 'PatientController');
-Route::resource('notifications', 'NotificationController');
-Route::resource('users', 'UserController');
-
-Route::get('change-password', 'ChangePasswordController@index')->name('passwords.index');
-Route::put('change-password', 'ChangePasswordController@update')->name('passwords.update');
-
+Route::group(['middleware' => 'auth'],function(){
+    Route::get('/', 'HomeController@index')->name('home');
+    Route::get('/patients/search/', 'PatientController@search')->name('patients.search');
+    Route::get('/patients/{id}/analyse','PatientController@analyse')->name('patients.analyse');
+    Route::resource('patients', 'PatientController');
+    Route::resource('notifications', 'NotificationController');
+    Route::resource('users', 'UserController');
+    Route::get('change-password', 'ChangePasswordController@index')->name('passwords.index');
+    Route::put('change-password', 'ChangePasswordController@update')->name('passwords.update');
+});
