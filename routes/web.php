ce488458f7886a6c0bdb079d2a5895f924b910ef
Kyle <kyle.ooi97@gmail.com>
2021-01-19 16:31:47 +0800

    added guest middleware for auth routes and added redirection with invalid url

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,26 @@ use App\Http\Controller\PatientController;
 |
 */
 
-Auth::routes();
+
+
+Route::group(['middleware' => 'guest'], function() {
+    // Authentication Routes...
+    Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+    Route::post('login', 'Auth\LoginController@login');
+
+
+    // Registration Routes...
+    Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+    Route::post('register', 'Auth\RegisterController@register');
+
+    // Password Reset Routes...
+    Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+    Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+    Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+    Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+});
+
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 
 Route::group(['middleware' => 'auth'],function(){
     Route::get('/', 'HomeController@index')->name('home');
@@ -26,3 +45,7 @@ Route::group(['middleware' => 'auth'],function(){
     Route::get('change-password', 'ChangePasswordController@index')->name('passwords.index');
     Route::put('change-password', 'ChangePasswordController@update')->name('passwords.update');
 });
+
+Route::any('{query}', 
+  function() { return redirect()->route('home')->withErrors(['Invalid URL entered!', 'Invalid URL']); })
+  ->where('query', '.*');
