87115a04e4beeed16f44fb8b37b8d7b047e40405
Kyle <kyle.ooi97@gmail.com>
2021-03-07 18:26:18 +0800

    Completed Login Test and RegistrationTest

--- database/factories/UserProfileFactory.php
+++ database/factories/UserProfileFactory.php
@@ -10,6 +10,7 @@ $factory->define(UserProfile::class, function (Faker $faker) {
         //
         'first_name' => $faker->firstName($gender = null),
         'last_name' => $faker->lastName(),
+        'ic_number' => '990292023390',
         'gender' => 'F',
         'contact' => '0123456789',
         'role' => '01',
